191f9772ff7534ee94b406317e9b3575c5dedc7e
sivavinod2004 <sivapriyaav2028@cs.sjcetpalai.ac.in>
2024-10-01 15:23:07 +0530

    document header added

---
+++ studentdetails.py
@@ -0,0 +1,15 @@
+'''
+#Author:siva
+#date:01\10\2024
+python program to get the student details
+version:1.0
+'''
+
+print("hello python")
+name=input("enter the student name")
+roll_number=int(input("enter roll number"))
+cgpa=float(input("enter cgpa"))
+percentage=cgpa*10
+print("name of the student",name)
+print("roll number is",roll_number)
+print("percentage is",percentage,"%")
