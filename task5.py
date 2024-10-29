4b2adc5a5dab97d88554c98d18039454fa9ff477
sivavinod2004 <sivapriyaav2028@cs.sjcetpalai.ac.in>
2024-10-29 15:26:18 +0530

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,1 +1,3 @@
+#sivapriya A V
+#CSE B
 print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are")
