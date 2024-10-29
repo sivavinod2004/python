a8ef0735725cd4a2abb1e111a5989b16dd135bbe
sivavinod2004 <sivapriyaav2028@cs.sjcetpalai.ac.in>
2024-10-29 15:17:42 +0530

    Add files via upload

---
+++ SIVA oct 29 3.py
@@ -0,0 +1,9 @@
+#sivapriya A V
+#CSE B
+#PROGRAM TO WRITE multiplication table
+number=int(input("enter a number"))
+for value in range(number,13):
+    m=number*value
+    print(number,"*",value,"=",m)
+
+
